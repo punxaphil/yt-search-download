1b616777c07c2e2d5d4296ed381fabefae214a83
Johan Frick <johan.frick@gmail.com>
2026-09-08 23:21:24 +0200

    cors allow

--- yt-search-download/CHANGELOG.md
+++ yt-search-download/CHANGELOG.md
@@ -1,6 +1,2 @@
-# 1.0.12
-- rw on share
-# 1.0.11
-- map share fix
 # 1.0.0
 - Initial release
--- yt-search-download/src/server.ts
+++ yt-search-download/src/server.ts
@@ -3,6 +3,13 @@ import { router } from './routes';
 
 const app = express();
 app.use(router);
+// allow all cors
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
+  next();
+});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
