b0ffdc12ae4918832ef6c04430d8778cfcbf8900
Johan Frick <johan.frick@gmail.com>
2026-09-08 23:28:04 +0200

    cors npm

--- yt-search-download/src/server.ts
+++ yt-search-download/src/server.ts
@@ -1,15 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import { router } from './routes';
 
 const app = express();
+
+app.use(cors()); // Allow all
 app.use(router);
-// allow all cors
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-  next();
-});
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
