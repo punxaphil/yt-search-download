a21b7eb33f62dfa1453897765be6ca4533ad2672
Johan Frick <johan.frick@gmail.com>
2026-09-15 23:25:26 +0200

    multiple results

--- yt-search-download/src/services/search.ts
+++ yt-search-download/src/services/search.ts
@@ -7,7 +7,7 @@ export async function searchYouTubeVideo(query: string) {
     '--print',
     '%(title)s\n%(webpage_url)s',
     '--skip-download',
-    `ytsearch1:${query}`,
+    `ytsearch10:${query}`,
   ]);
   console.log(`yt-dlp search result for query "${query}":`, commandResult.stdout);
   const lines = commandResult.stdout
